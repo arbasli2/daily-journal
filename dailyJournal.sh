54c8d38a152f11dffb0518174b4d1a9d95e0227d
Reza <arbasli2@users.noreply.github.com>
2016-12-15 10:02:21 +0100

    first commit

---
+++ dailyJournal.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+# usage 
+#dailyJournal   # journal of today
+#dailyJournal  2   # it shows the journal of 2 days ago
+#dailyJournal -d 20161010  # show the journal of the date 20161010 
+
+
+if [ $# -eq 0 ]; then
+	DAYS=0
+	DA=`date --date="$DAYS days ago" +%Y%m%d%a`
+else
+	if [ $1 = "-d"   ]; then
+		DA=`date --date="$2" +%Y%m%d%a`
+	else
+		DAYS=$1
+		DA=`date --date="$DAYS days ago" +%Y%m%d%a`
+	fi
+fi
+
+FILENAME=~/Dropbox/Notes/journal_commandline/daily$DA.txt 
+if [  -f $FILENAME ]; then
+	vim $FILENAME  -c 'set syntax=markdown'
+else
+	touch $FILENAME
+	echo **Journal for the day:  $DA**  > $FILENAME 
+	echo >> $FILENAME
+	echo >> $FILENAME
+	echo  \# scratch >> $FILENAME
+	vim $FILENAME  -c 'set syntax=markdown'
+fi
+
