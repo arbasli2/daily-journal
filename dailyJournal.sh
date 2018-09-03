f387e871d0d8eaaa491c8ab5904a96c090155936
Reza <arbasli2@users.noreply.github.com>
2018-09-03 22:19:54 +0200

    minor

--- dailyJournal.sh
+++ dailyJournal.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# creates an template and opens vim for editing it. the journal is supposed to be written in RestructuredText format
+# creates a template and opens vim for editing it. The journal is supposed to be written in reStructuredText format.
 # usage 
 #dailyJournal   # journal of today
 #dailyJournal  2   # it shows the journal of 2 days ago
